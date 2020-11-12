38b7e1557bdfab3db4fc7e08a04c779e1c6e79e6
Jason Fujii <Jasonfujii@yahoo.com>
2020-11-12 03:13:32 -0800

    Part B completed except for immediate recursion functions

---
+++ cmake-build-debug/CMakeFiles/Assignment_04_Code.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jasonfujii/Desktop/Code/CSC340 - Programming Methodology/Assignment-04-Code/LinkedBag.cpp" "/Users/jasonfujii/Desktop/Code/CSC340 - Programming Methodology/Assignment-04-Code/cmake-build-debug/CMakeFiles/Assignment_04_Code.dir/LinkedBag.cpp.o"
+  "/Users/jasonfujii/Desktop/Code/CSC340 - Programming Methodology/Assignment-04-Code/LinkedBag340.cpp" "/Users/jasonfujii/Desktop/Code/CSC340 - Programming Methodology/Assignment-04-Code/cmake-build-debug/CMakeFiles/Assignment_04_Code.dir/LinkedBag340.cpp.o"
+  "/Users/jasonfujii/Desktop/Code/CSC340 - Programming Methodology/Assignment-04-Code/LinkedBagClient340.cpp" "/Users/jasonfujii/Desktop/Code/CSC340 - Programming Methodology/Assignment-04-Code/cmake-build-debug/CMakeFiles/Assignment_04_Code.dir/LinkedBagClient340.cpp.o"
+  "/Users/jasonfujii/Desktop/Code/CSC340 - Programming Methodology/Assignment-04-Code/Node.cpp" "/Users/jasonfujii/Desktop/Code/CSC340 - Programming Methodology/Assignment-04-Code/cmake-build-debug/CMakeFiles/Assignment_04_Code.dir/Node.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Assignment_04_Code.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Assignment_04_Code"
+  "Assignment_04_Code.pdb"
+  "CMakeFiles/Assignment_04_Code.dir/LinkedBag.cpp.o"
+  "CMakeFiles/Assignment_04_Code.dir/LinkedBag340.cpp.o"
+  "CMakeFiles/Assignment_04_Code.dir/LinkedBagClient340.cpp.o"
+  "CMakeFiles/Assignment_04_Code.dir/Node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_04_Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
